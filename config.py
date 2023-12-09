4945c14ec8f409490ef9d4aa9539db5e7675bb04
Mr King <140832898+KING0712@users.noreply.github.com>
2023-12-09 10:43:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 200))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 1225691930))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KING0712/KINGXMUSIC_BOT",
+    "@l_MR_ll_KING_l",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/l_Lovely_Duniya_l")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/l_Lovely_Duniya_l")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "@l_MR_ll_KING_l")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "@l_MR_ll_KING_l")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -52,7 +52,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 100))
 
 
 # Telegram audio and video file size limit (in bytes)
@@ -78,26 +78,26 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/09ec50364efd95e811b93.jpg"
+    "START_IMG_URL", "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/47b26b2859ccf4d58b318.jpg"
+    "PING_IMG_URL", "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+STATS_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+STREAM_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/9a6da16d52fdb628901a6.jpg"
 
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+    return sum(int(x) * 200**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
